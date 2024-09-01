96b9996528f62124eed6b46db7d6a27ba9b2f19a
Abhijit Jha <abhijitjha1801@gmail.com>
2024-09-01 15:05:01 +0530

    added remove feature
    to remove the task from the list

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ let Button=document.querySelector("Button");
 let Ul=document.querySelector("Ul");
 Button.addEventListener("click",function(){
     let Task=document.createElement("li");
+    let dltbttn=document.createElement("button");
+    dltbttn.innerText="remove";
+    dltbttn.classList.add("remove");
     let Name=Input.value;
     Name = Name.replace(/[^a-zA-Z\s]/g, '');
     Task.innerText=Name;
+    Task.appendChild(dltbttn);
     Ul.appendChild(Task);
     Input.value="";
 });
+Ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem=event.target.parentElement;
+        listItem.remove();
+    }
+})
